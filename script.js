933311145cc8bddca019eb0d8f761da4144249b8
Domilvio Júlio Gonçalves Barroso <domilviojuliogoncalvesbarroso@gmail.com>
2025-04-01 16:16:10 +0100

    Add files via upload
    Feito

--- script.js
+++ script.js
@@ -19,6 +19,14 @@ function converterMoeda(){
        coin.innerHTML=parseFloat(valor) + " $ equivale a " + parseFloat(resultado2.toPrecision(4)) + "kz";
    
     }
+    else if(moeda.value=="real"){
+       valor=valor.replace(/\./g,"").replace(",",".");
+        resultado1=valor * 0.0062;
+        resultado2=valor / 0.0062;
+        mostrar.innerHTML=valor + " kz equivale a " + parseFloat(resultado1.toPrecision(4))  + "R$";
+       coin.innerHTML=parseFloat(valor) + " R$ equivale a " + parseFloat(resultado2.toPrecision(4)) + "kz";
+   
+    }
    else if(moeda.value=="libra"){
        valor=valor.replace(/\./g,"").replace(",",".");
         resultado1=valor * 0.00085;
