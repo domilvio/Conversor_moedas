dd67037719e808d24f94259b43700e8eaa132dee
Domilvio Júlio Gonçalves Barroso <domilviojuliogoncalvesbarroso@gmail.com>
2025-04-01 15:55:30 +0100

    Add files via upload
    Feito

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 function converterMoeda(){
     let valor = document.querySelector("input").value;
     let moeda = document.querySelector("#moeda");
-    let resultado = document.querySelector("p");
+    let mostrar = document.querySelector("p");
     let coin = document.querySelector("#coin");
    
     if(moeda.value=="euro"){
-        resultado.innerHTML=valor + " kz equivale a " + valor * 0.0010 + "€";
-        coin.innerHTML=valor + " € equivale a " + valor * 0.0010 + "kz";
+        valor=valor.replace(/\./g,"").replace(",",".");
+        resultado1=valor * 0.0010;
+        resultado2=valor / 0.0010;
+        mostrar.innerHTML=valor + " kz equivale a " + parseFloat(resultado1.toPrecision(4)) + "€";
+        coin.innerHTML=parseFloat(valor) + " € equivale a " + parseFloat(resultado2.toPrecision(4)) + "kz";
     }
    else if(moeda.value=="dolar"){
-        resultado.innerHTML=valor + " kz equivale a " + valor * 0.0011 + "$";
-       coin.innerHTML=valor + " $ equivale a " + valor * 0.0011 + "kz";
+       valor=valor.replace(/\./g,"").replace(",",".");
+       resultado1=valor * 0.0011;
+       resultado2=valor / 0.0011;
+       mostrar.innerHTML=valor + " kz equivale a " + parseFloat(resultado1.toPrecision(4)) + "$";
+       coin.innerHTML=parseFloat(valor) + " $ equivale a " + parseFloat(resultado2.toPrecision(4)) + "kz";
    
     }
    else if(moeda.value=="libra"){
-        resultado.innerHTML=valor + " kz equivale a " + valor * 0.00085 + "£";
-       coin.innerHTML=valor + " £ equivale a " + valor * 0.00085 + "kz";
+       valor=valor.replace(/\./g,"").replace(",",".");
+        resultado1=valor * 0.00085;
+        resultado2=valor / 0.00085;
+        mostrar.innerHTML=valor + " kz equivale a " + parseFloat(resultado1.toPrecision(4))  + "£";
+       coin.innerHTML=parseFloat(valor) + " £ equivale a " + parseFloat(resultado2.toPrecision(4)) + "kz";
    
     }
     else if(moeda.value=="metical"){
-        resultado.innerHTML=valor + " kz equivale a " + valor * 0.070 + "MT";
-        coin.innerHTML=valor + " MT equivale a " + valor * 0.070 + "kz";
+        valor=valor.replace(/\./g,"").replace(",",".");
+        resultado1=valor * 0.070;
+        resultado2=valor / 0.070;
+        mostrar.innerHTML=valor + " kz equivale a " + parseFloat(resultado1.toPrecision(4)) + "MT";
+        coin.innerHTML=parseFloat(valor) + " MT equivale a " + parseFloat(resultado2.toPrecision(4)) + "kz";
    
     }
     }
