e56ce726bbb53b361e7d843b2d1a8e01c9c69a05
Domilvio Júlio Gonçalves Barroso <domilviojuliogoncalvesbarroso@gmail.com>
2025-04-01 12:01:48 +0100

    Add files via upload
    Feito

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ function converterMoeda(){
     let valor = document.querySelector("input").value;
     let moeda = document.querySelector("#moeda");
     let resultado = document.querySelector("p");
+    let coin = document.querySelector("#coin");
+   
     if(moeda.value=="euro"){
         resultado.innerHTML=valor + " kz equivale a " + valor * 0.0010 + "€";
+        coin.innerHTML=valor + " € equivale a " + valor * 0.0010 + "kz";
     }
    else if(moeda.value=="dolar"){
         resultado.innerHTML=valor + " kz equivale a " + valor * 0.0011 + "$";
+       coin.innerHTML=valor + " $ equivale a " + valor * 0.0011 + "kz";
+   
     }
    else if(moeda.value=="libra"){
         resultado.innerHTML=valor + " kz equivale a " + valor * 0.00085 + "£";
+       coin.innerHTML=valor + " £ equivale a " + valor * 0.00085 + "kz";
+   
     }
     else if(moeda.value=="metical"){
         resultado.innerHTML=valor + " kz equivale a " + valor * 0.070 + "MT";
+        coin.innerHTML=valor + " MT equivale a " + valor * 0.070 + "kz";
+   
     }
     }
 
